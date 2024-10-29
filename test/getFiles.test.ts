a1ce240c40f24ba9bde19076a493d3444eecfafc
MrProgrammerMan <jonas.baugerud@gmail.com>
2024-10-29 18:27:01 +0100

    refactor(test): refactor for readability and structure

--- test/getFiles.test.ts
+++ test/getFiles.test.ts
@@ -1,13 +1,16 @@
-import { expect, test } from 'vitest'
-import getFiles from '../src/helpers/getFiles'
+import { expect, describe, test } from 'vitest';
+import getFiles from '../src/helpers/getFiles';
 
-test('getFiles', () => {
-  const tsFile = getFiles('test/assets', ['.ts']);
-  expect(tsFile).toHaveLength(1);
-  expect(tsFile).toContain('index.ts');
-  const files = getFiles('test/assets', [".jpg", ".webp"]);
-  expect(files).toHaveLength(3);
-  expect(files).toContain("jpg0.jpg");
-  expect(files).toContain("webp0.webp");
-  expect(files).toContain("webp1.webp");
-})
+describe('getFiles', () => {
+  test('should return only .ts files', () => {
+    const tsFiles = getFiles('test/assets', ['.ts']);
+    expect(tsFiles).toHaveLength(1);
+    expect(tsFiles).toContain('index.ts');
+  });
+
+  test('should return .jpg and .webp files', () => {
+    const imageFiles = getFiles('test/assets', ['.jpg', '.webp']);
+    expect(imageFiles).toHaveLength(3);
+    expect(imageFiles).toEqual(expect.arrayContaining(['jpg0.jpg', 'webp0.webp', 'webp1.webp']));
+  });
+});
