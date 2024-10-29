1f1b74f80e012796ad48b8629fa4bb6388bdf69b
MrProgrammerMan <jonas.baugerud@gmail.com>
2024-10-29 18:32:51 +0100

    refactor(helper): specify typescript type of return

--- src/helpers/getFiles.ts
+++ src/helpers/getFiles.ts
@@ -8,7 +8,7 @@ import path from "path";
  * @param {string[]} fileTypes - An array of file extensions (e.g., ['.jpg', '.png']) to filter the files by.
  * @returns {string[]} - An array of file names with the matching extensions found in the directory.
  */
-export default function getFiles (dir: string, fileTypes : string[]) {
+export default function getFiles (dir: string, fileTypes : string[]) : string[] {
   return fs.readdirSync(dir) // Synchronously read the contents of the directory
     .filter((file: string) => fileTypes.includes(path.extname(file).toLowerCase())); // Only include files with matching extension
 };
