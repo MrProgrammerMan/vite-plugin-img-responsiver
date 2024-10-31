221f7f016bb8c952ac7537c71774a93c3e030963
Wizard764 <jonasbau@stud.ntnu.no>
2024-10-31 17:18:39 +0100

    chore!(cleanup): remove entirely unneccessary log method.

--- src/helpers/log.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Logs a message to the console.
- * 
- * @param {string} message - The message to log to the console.
- */
-export default function log (message: string) {
-  console.log(message);
-}
