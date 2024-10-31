89fe6eaea55f1038d514f0c1279d7cbd834d7504
MrProgrammerMan <jonas.baugerud@gmail.com>
2024-10-31 10:10:47 +0100

    refactor(test): getFiles.test creates test assets rather than relying on pre-existing assets

--- test/getFiles.test.ts
+++ test/getFiles.test.ts
@@ -1,16 +1,35 @@
-import { expect, describe, test } from 'vitest';
+import { expect, describe, test, beforeAll, afterAll } from 'vitest';
 import getFiles from '../src/helpers/getFiles';
+import fs from 'fs';
 
 describe('getFiles', () => {
+  // Generate test files before attmepting to read them using fs
+  const testDir = 'test/getFiles';
+  beforeAll(() => {
+    const files = ['index.ts', 'jpg0.jpg', 'webp0.webp', 'webp1.webp'];
+
+    if (!fs.existsSync(testDir)) {
+      fs.mkdirSync(testDir);
+    }
+
+    files.forEach((file) => {
+      fs.writeFileSync((testDir + "/" + file), '');
+    });
+  });
+
   test('should return only .ts files', () => {
-    const tsFiles = getFiles('test/assets/getfiles', ['.ts']);
+    const tsFiles = getFiles(testDir, ['.ts']);
     expect(tsFiles).toHaveLength(1);
     expect(tsFiles).toContain('index.ts');
   });
 
   test('should return .jpg and .webp files', () => {
-    const imageFiles = getFiles('test/assets/getfiles', ['.jpg', '.webp']);
+    const imageFiles = getFiles(testDir, ['.jpg', '.webp']);
     expect(imageFiles).toHaveLength(3);
     expect(imageFiles).toEqual(expect.arrayContaining(['jpg0.jpg', 'webp0.webp', 'webp1.webp']));
+  });
+
+  afterAll(() => {
+    fs.rm(testDir, { recursive: true }, () => {});
   });
 });
