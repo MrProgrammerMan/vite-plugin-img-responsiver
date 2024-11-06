266e44baa4de718f88bf5a0a82a2d6038f609e04
Wizard764 <jonasbau@stud.ntnu.no>
2024-11-06 11:42:33 +0100

    feat!(main): implement main plugin logic. The plugin now functions in dev mode

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,114 @@
-import { Plugin } from 'rollup';
-import Config from './config';
-import defaults from './defaults';
-import processImages from './helpers/image-handling';
-import fs from 'fs';
+import { Plugin } from "rollup";
+import Config from "./config";
+import defaults from "./defaults";
+import processImages from "./helpers/image-handling";
+import fs from "fs";
+import getFiles from "./helpers/getFiles";
+import processHtml from "./helpers/html-handling";
 
-export default function imgResponsiver(userConfig: Partial<Config> = {}): Plugin {
-  console.log('Running img-responsiver plugin...');
-
+export default function imgResponsiver(
+  userConfig: Partial<Config> = {}
+): Plugin {
+  console.log("Running img-responsiver plugin...");
 
   // Merge user configuration with default configuration
   const config: Config = { ...defaults, ...userConfig };
+  console.log("Configuration: ");
   console.log(config);
 
-  // Convert all ambiguous configuration values to arrays
-  config.imagesDirs = Array.isArray(config.imagesDirs) ? config.imagesDirs : [config.imagesDirs];
-  config.htmlDirs = Array.isArray(config.htmlDirs) ? config.htmlDirs : [config.htmlDirs];
-
   return {
-    name: 'img-responsiver',
-    version: '1.0.0',
+    name: "img-responsiver",
+    version: "1.0.0",
     async buildStart() {
+      // Create output directory if it doesn't exist
       if (!fs.existsSync(config.outputDir)) {
         fs.mkdirSync(config.outputDir);
       }
+      console.log(`Outputting to directory: ${config.outputDir}`);
 
-      let hashes: string[] = [];
+      // Ensure that imagesDirs and htmlDirs are arrays. This is in case user has only specified a single directory
+      config.imagesDirs = Array.isArray(config.imagesDirs)
+        ? config.imagesDirs
+        : [config.imagesDirs];
 
-      for (const imageDir of config.imagesDirs) {
-        console.log(`Processing images in directory: ${imageDir}`);
-        const hashesOut = await processImages(imageDir, config.imageExtensions, config.conversionSizes, config.outputFileTypes, config.outputDir);
-        hashes = hashes.concat(hashesOut);
-      }
+      const htmlDirsArray = Array.isArray(config.htmlDirs)
+        ? config.htmlDirs
+        : [config.htmlDirs];
 
-      console.log('Processed all images.');
-      console.log(hashes);
-    }
+      console.log(`Image Directories: ${config.imagesDirs}`);
+      const imgTasks: Promise<string[]>[] = config.imagesDirs.map(
+        async (imageDir) => {
+          console.log(`\tGenerating image variants for directory: ${imageDir}`);
+          return await processImages(
+            imageDir,
+            config.imageExtensions,
+            config.conversionSizes,
+            config.outputFileTypes,
+            config.outputDir
+          );
+        }
+      );
+
+      const allHashes = (await Promise.all(imgTasks)).flat();
+      console.log(
+        `Generated image variants for all directories: ${config.imagesDirs}`
+      );
+      console.log(`Generated image hashes: ${allHashes}`);
+
+      console.log(`HTML Directories: ${htmlDirsArray}`);
+      const htmlTasks: Promise<void>[] = htmlDirsArray.flatMap((htmlDir) => {
+        console.log(`Processing HTML files in directory: ${htmlDir}`);
+        return getFiles(htmlDir, [config.htmlFileType]).map(
+          async (htmlFile) => {
+            console.log(`\tProcessing HTML file: ${htmlFile}`);
+            processHtml(
+              htmlDir,
+              htmlFile,
+              config.imgTagRegex,
+              allHashes,
+              config.outputDir,
+              config.conversionSizes,
+              config.outputFileTypes
+            );
+          }
+        );
+      });
+
+      await Promise.all(htmlTasks);
+      console.log("Processed all HTML files in directories: ", htmlDirsArray);
+    },
+    async closeBundle() {
+      const htmlDirsArray = Array.isArray(config.htmlDirs)
+        ? config.htmlDirs
+        : [config.htmlDirs];
+
+      // <picture> tag regex that captures internal <img> tag
+      const pictureTagRegex = /<picture>(?:\s*<source[^>]*>\s*)*(<img[^>]*>)\s*<\/picture>/g;
+
+      console.log("Cleaning up html...");
+      console.log(`HTML Directories: ${htmlDirsArray}`);
+      const htmlTasks: Promise<void>[] = htmlDirsArray.flatMap((htmlDir) => {
+        console.log(`Cleaning up HTML files in directory: ${htmlDir}`);
+        return getFiles(htmlDir, [config.htmlFileType]).map(
+          async (htmlFile) => {
+            console.log(`\tCleaning HTML file: ${htmlFile}`);
+            
+            // Get file content
+            let htmlContent = fs.readFileSync(`${htmlDir}/${htmlFile}`, "utf8");
+            // Find all picture tags in the html file
+            const matches = htmlContent.matchAll(pictureTagRegex);
+            for (const match of matches) {
+              const imgTag = match[1]; // <img> tag
+              const pictureTag = match[0]; // <picture> tag
+              htmlContent = htmlContent.replace(pictureTag, imgTag);
+            }
+            fs.writeFileSync(`./${htmlDir}/${htmlFile}`, htmlContent);
+          }
+        );
+      });
+
+      await Promise.all(htmlTasks);
+      console.log("Cleaned up all HTML files in directories: ", htmlDirsArray);
+    },
   };
 }
