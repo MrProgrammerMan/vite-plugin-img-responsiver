1de4ae8dc8f5adac8db90585b8e983bcad81c148
MrProgrammerMan <jonas.baugerud@gmail.com>
2024-11-06 18:57:36 +0100

    feat(example): improve example project by adding more images and styling. Also some explanatory text.

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,5 +2,16 @@ import { defineConfig } from 'vite';
 import imgResponsiver from 'rollup-plugin-img-responsiver'; // Import your plugin
 
 export default defineConfig({
-  plugins: [imgResponsiver()]
+  plugins: [
+    imgResponsiver({
+      imagesDirs: ["./src/assets/imgs"],
+      imageExtensions: [".png", ".jpg", ".jpeg", ".gif", ".webp"],
+      conversionSizes: [240, 480, 768, 1280, 1920],
+      outputFileTypes: [".avif", ".webp", ".jpg"],
+      outputDir: "img-responsiver-output",
+      htmlDirs: ["./", "./src/assets/html"],
+      htmlFileType: ".html",
+      imgTagRegex: /<img\s+[^>]*src=["']([^"']+)["'][^>]*>/g,
+    })
+  ]
 });
