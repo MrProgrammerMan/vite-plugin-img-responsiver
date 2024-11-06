0587f38cce2667cf369769f856a9aa99ffc1cbc3
Wizard764 <jonasbau@stud.ntnu.no>
2024-11-06 08:29:48 +0100

    feat(config): minor adjustment to defaults

--- src/defaults.ts
+++ src/defaults.ts
@@ -4,7 +4,7 @@ import Config from './config';
  * Default configuration values for the image responsiver plugin.
  */
 const defaults: Config = {
-  imagesDirs: "src/assets/imgs",
+  imagesDirs: ["src/assets/imgs"],
   imageExtensions: [".png", ".jpg", ".jpeg", ".gif", ".webp"],
   conversionSizes: [240, 480, 768, 1280, 1920],
   outputFileTypes: [".avif", ".webp", ".jpg"],
